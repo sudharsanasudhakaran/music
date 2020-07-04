b44a4662a13955614743d77abf85b5952f452bbf
Sudharsana <sudharsana55@gmail.com>
2020-07-04 22:23:34 +0530

    Add files via upload

---
+++ test.ts
@@ -0,0 +1,155 @@
+interface now_playing{
+    querySelector = ATTRIBUTE_NODE("now-playing");
+}
+interface track_art {
+    querySelector = ATTRIBUTE_NODE(".track-art");
+}
+let track_name = document.querySelector(".track-name");
+interface track_name {
+    querySelector = ATTRIBUTE_NODE(".track_name");
+}
+interface track_artist {
+    querySelector = ATTRIBUTE_NODE(".track_artist");
+}
+interface genre {
+    querySelector = ATTRIBUTE_NODE(".genre");
+}
+let playpause_btn = document.querySelector(".playpause-track");
+interface playpause_btn {
+    querySelector = ATTRIBUTE_NODE("playpause-track");
+}
+
+// Create new audio element
+let curr_track = document.createElement('audio');
+interface curr_track {
+    querySelector = ATTRIBUTE_NODE("curr_track");
+  
+}
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "Memories",
+    artist: "Maroon 5",
+    genre: "pop",
+    image: "https://upload.wikimedia.org/wikipedia/en/c/c3/Maroon_5_-_Memories.png",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
+  },
+  {
+    name: "Bad Guy",
+    artist: "Billie Ellish",
+    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
+  },
+  {
+    name: "Closer",
+    artist: "Chainsmokers",
+    image: "https://runthetrap.com/wp-content/uploads/2016/09/Closer-Remix-Artwork-1024x1024.png",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
+  },
+  {
+    name: "Numb",
+    artist: "Linkin Park",
+    image: "https://bhavyasm.files.wordpress.com/2018/07/download.jpg",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3",
+  },
+  {
+    name: "Love Story",
+    artist: "Taylor Swift",
+    image: "https://upload.wikimedia.org/wikipedia/en/0/01/Taylor_Swift_-_Love_Story.png",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
+
+  },
+];
+
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  resetValues();
+  curr_track.src = track_list[track_index].path;
+  curr_track.load();
+
+  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+  track_name.textContent = track_list[track_index].name;
+  track_artist.textContent = track_list[track_index].artist;
+  genre.textContent - track_list[track_index].genre;
+  
+
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+  random_bg_color();
+}
+
+function resetValues() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+
+// Load the first track in the tracklist
+loadTrack(track_index);
+
+function playpauseTrack() {
+  if (!isPlaying) playTrack();
+  else pauseTrack();
+}
+
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
+}
+
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+}
+
+function nextTrack() {
+  if (track_index < track_list.length - 1)
+    track_index += 1;
+  else track_index = 0;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function prevTrack() {
+  if (track_index > 0)
+    track_index -= 1;
+  else track_index = track_list.length;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function seekTo() {
+  seekto = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekto;
+}
+
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+function seekUpdate() {
+  let seekPosition = 0;
+
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
+
+
